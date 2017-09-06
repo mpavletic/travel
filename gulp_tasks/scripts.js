32576ad4a360c4210b1a325ad7bd6d27aa5f9f6b
Martin Pavletic <tinchopavl@gmail.com>
2017-09-05 22:50:35 -0300

    Change scripts order on inject task

--- gulp_tasks/scripts.js
+++ gulp_tasks/scripts.js
@@ -6,7 +6,7 @@ const conf = require('../conf/gulp.conf');
 gulp.task('scripts', scripts);
 
 function scripts() {
-  return gulp.src(conf.path.src('**/*.js'))
+  return gulp.src(['index.js', conf.path.src('**/*.js')])
     .pipe(eslint())
     .pipe(eslint.format())
 
